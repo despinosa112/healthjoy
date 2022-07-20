b340f20035aaedc94fb0b7c5a4f1bed6813a20f4
Daniel Espinosa <despinosa112@gmail.com>
2022-07-20 18:27:14 -0400

    Initial Commit

---
+++ HealthJoy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthJoy
+//
+//  Created by Daniel Espinosa on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthJoy/HealthJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthJoyApp.swift
+//  HealthJoy
+//
+//  Created by Daniel Espinosa on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthJoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
